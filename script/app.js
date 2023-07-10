6f3942afd6d95fb4cf547c9154651469f5f101a8
23mirkovic35 <mirkovic.mire@outlook.com>
2023-07-10 13:46:35 +0200

    wokring on writing text on canvas

--- script/app.js
+++ script/app.js
@@ -4,6 +4,8 @@ const checkBoxArray = document.querySelectorAll('input');
 let selectedItem = "pointer";
 let lastSelectedItemArray = new Array(3);
 
+var selectedColor;
+
 function initLastSelectedItemArray(){
     lastSelectedItemArray[0] = "pointer";
     lastSelectedItemArray[1] = "pencile";
@@ -169,28 +171,126 @@ document.addEventListener('keyup', function(event) {
         else if(event.key == 'L'){
             activeMenuItem(3,0);
         }
+        else if(event.key == 'C'){
+            checkBoxArray[6].checked = true;
+
+        }
     }
     shiftPressed = false;
 });
 
+function setColor(index){
+    const colorItem = document.getElementById("current-color");
+    switch (index) {
+        case 1:
+            selectedColor = "black";
+            colorItem.style.backgroundColor="#252525";
+            break;
+        case 2: 
+            selectedColor = "yellow";
+            colorItem.style.backgroundColor="rgb(254, 212, 48)";
+            break;
+        case 3:
+            selectedColor = "light-orange";
+            colorItem.style.backgroundColor="rgb(251, 174, 23)";
+            break;
+        case 4:
+            selectedColor = "orange";
+            colorItem.style.backgroundColor="rgb(243, 99, 35)";
+            break;
+        case 5:
+            selectedColor = "red";//
+            colorItem.style.backgroundColor="rgb(191,0,0)";
+            break;
+        case 6:
+            selectedColor = "dark-red";
+            colorItem.style.backgroundColor="rgb(128,0,0)";
+            break;   
+        case 7:
+            selectedColor = "magenta";
+            colorItem.style.backgroundColor="rgb(207, 18, 120)";
+            break;
+        case 8:
+            selectedColor = "dark-purple";
+            colorItem.style.backgroundColor="rgb(91, 49, 141)";
+            break; 
+        case 9:
+            selectedColor = "purple";
+            colorItem.style.backgroundColor="rgb(145, 75, 184)";
+            break; 
+        case 10:
+            selectedColor = "light-blue";
+            colorItem.style.backgroundColor="rgb(62, 204, 253)";
+            break; 
+        case 11:
+            selectedColor = "blue";
+            colorItem.style.backgroundColor="rgb(0, 105, 253)";
+            break; 
+        case 12:
+            selectedColor = "light-green";
+            colorItem.style.backgroundColor="rgb(126, 196, 0)";
+            break; 
+        case 13:
+            selectedColor = "green";
+            colorItem.style.backgroundColor="rgb(2, 165, 86)";
+            break; 
+        case 14:
+            selectedColor = "light-gray";
+            colorItem.style.backgroundColor="rgb(235, 235, 235)";
+            break; 
+        case 15:
+            selectedColor = "gray";
+            colorItem.style.backgroundColor="rgb(182, 182, 182)";
+            break; 
+
+        default:
+            break;
+    }
+    checkBoxArray[6].checked = false;
+}
+
+function getColor(){
+    switch(selectedColor){
+        case "black": return "#252525";
+        case "yellow": return "rgb(254, 212, 48)";
+        case "light-orange": return "rgb(251, 174, 23)";
+        case "orange": return "rgb(251, 174, 23)";
+        case "red":return "rgb(191,0,0)"
+        case "dark-red":return "rgb(128,0,0)"
+        case "magenta":return "rgb(207, 18, 120)"
+        case "dark-purple":return "rgb(91, 49, 141)"
+        case "purple":return "rgb(145, 75, 184)"
+        case "light-blue": return "rgb(62, 204, 253)"
+        case "blue": return "rgb(0, 105, 253)";
+        case "light-green": return "rgb(126, 196, 0)"
+        case "green": return "rgb(2, 165, 86)";
+        case "light-gray": return "rgb(235, 235, 235)"
+        case "gray": return "rgb(182, 182, 182)";
+        default: break;
+    }
+    return "";
+}
+
+
 // drawing on canvas
 
-const main = document.getElementById('main');
-const mctx = main.getContext('2d')
+var draft = document.getElementById('draft')
+var ctx = draft.getContext('2d')
+draft.height = window.innerHeight
+draft.width = window.innerWidth
+var main = document.getElementById('main')
+var mctx = main.getContext('2d')
 main.height = window.innerHeight
 main.width = window.innerWidth
-const draft = document.getElementById('draft');
-const ctx = draft.getContext('2d');
-draft.height = window.innerHeight;
-draft.width = window.innerWidth;
 
-var isDrawing = false;
+var action = "none";
 var prev;
+var alpha = 0.4;
 
 function getXY(e) {
     var r = draft.getBoundingClientRect();
     return {x: e.clientX - r.left, y: e.clientY - r.top}
-  }
+}
 
 draft.addEventListener('mousedown', handleMouseDown);
 draft.addEventListener('mousemove', handleMouseMove);
@@ -198,6 +298,7 @@ draft.addEventListener('mouseup', handleMouseUp);
 
 
 function drawPen(event){
+    ctx.strokeStyle = getColor();
     ctx.lineWidth = 1;                 
     var point = getXY(event)
     ctx.beginPath()
@@ -208,16 +309,116 @@ function drawPen(event){
     prev = point
 }
 
+function drawMarker(event){
+    ctx.strokeStyle = getColor();
+    ctx.lineWidth = 5;
+    draft.style.opacity = 1        
+    mctx.globalAlpha = 1                
+    var point = getXY(event)
+    ctx.beginPath()            
+    ctx.moveTo(prev.x, prev.y)      
+    ctx.lineTo(point.x, point.y)
+    ctx.stroke()  
+    ctx.closePath()                
+    prev = point
+}
+
+
+function drawHighlighter(e){
+    ctx.strokeStyle = getColor();
+    ctx.lineWidth = 16;
+    ctx.lineCap = "round";
+    draft.style.opacity = alpha;                        
+    mctx.globalAlpha = alpha;                           
+    var point = getXY(e);
+    ctx.beginPath();                                  
+    ctx.moveTo(prev.x, prev.y);                       
+    ctx.lineTo(point.x, point.y);                     
+    ctx.stroke();                                     
+    prev = point;   
+}
+
+function drawRect(e){
+    const point = getXY(e)
+    ctx.clearRect(0, 0, draft.width, draft.height)
+    width = point.x - prev.x;
+    height = point.y - prev.y;
+    ctx.beginPath()
+    ctx.rect(prev.x, prev.y, width, height);
+    ctx.stroke()
+    ctx.closePath()
+}
+
+function drawCircle(e){
+    ctx.strokeStyle = getColor();
+    const point = getXY(e)
+    radius = Math.sqrt(Math.pow(point.x - prev.x, 2) + Math.pow(point.y - prev.y, 2))
+    ctx.clearRect(0, 0, draft.width, draft.height)
+    ctx.beginPath()
+    ctx.arc(prev.x, prev.y, radius, 0, 2 * Math.PI)
+    ctx.stroke()
+    ctx.closePath()
+}
+
+function drawStraightLine(e){
+    ctx.strokeStyle = getColor();
+    const point = getXY(e)
+    ctx.clearRect(0, 0, draft.width, draft.height)
+    width = point.x - prev.x;
+    height = point.y - prev.y;
+    ctx.beginPath()
+    ctx.moveTo(prev.x, prev.y)                 
+    ctx.lineTo(point.x, point.y)
+    ctx.stroke()
+    ctx.closePath()
+}
+
+
+const handleBlue = () =>{
+    console.log("text is set");
+}
+
+
 function handleMouseDown(event) {
-  isDrawing = true;
+    if(selectedItem == "pointer") action = "moving";
+    else if(selectedItem!="text") action = "drawing";
+    else {
+        action = "writing";
+        const point = getXY(event);
+        const textarea = document.getElementById("textarea");
+        textarea.style.visibility = "visible";
+        textarea.style.top = point.y + "px";
+        textarea.style.left = point.x + "px";   
+    }
   prev = getXY(event);
 }
 
+
 function handleMouseMove(event) {
-    if(!isDrawing) return;
+    if(action=="none") return;
     switch(selectedItem){
         case "pencile":{
             drawPen(event);
+            break;
+        }
+        case "marker":{
+            drawMarker(event);
+            break;
+        }
+        case "highlighter":{
+            drawHighlighter(event);
+            break;
+        }
+        case "rectangle":{
+            drawRect(event);
+            break;
+        }
+        case "circle":{
+            drawCircle(event);
+            break;
+        }
+        case "line":{
+            drawStraightLine(event);
             break;
         }
         default:break;
@@ -225,7 +426,10 @@ function handleMouseMove(event) {
 }
 
 function handleMouseUp() {
-  isDrawing = false;
+    if(action=="writing") return;
+    mctx.drawImage(draft, 0, 0);
+    ctx.clearRect(0,0,draft.width,draft.height);
+    action = "none";
 }
 
 draft.addEventListener("click", ()=>{
